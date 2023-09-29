f0245143f4896a3e80bc3157963f89e8b5bfedea
unanchoi <unanc2@gmail.com>
2023-09-29 22:38:42 +0900

    [WEEK1] health check

--- src/main/java/com/example/seminar/controller/HealthCheckController.java
+++ src/main/java/com/example/seminar/controller/HealthCheckController.java
@@ -1,6 +1,7 @@
 package com.example.seminar.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,19 @@ import java.util.Map;
 public class HealthCheckController {
 
     @GetMapping("/v1")
-    public Map<String, String> healthCheck() {
+    public Map<String, String> healthCheckV1() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "OK");
         return response;
+    }
+
+    @GetMapping("/v2")
+    public ResponseEntity<String> healthCheckV2() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/v3")
+    public ResponseEntity<HealthCheckResponse> healthCheckV3() {
+        return ResponseEntity.ok(new HealthCheckResponse("OK"));
     }
 }
---
+++ src/main/java/com/example/seminar/controller/HealthCheckResponse.java
@@ -0,0 +1,9 @@
+package com.example.seminar.controller;
+
+public class HealthCheckResponse {
+    private String status;
+
+    public HealthCheckResponse(String status) {
+        this.status = status;
+    }
+}
